1127d194f3c5cab913214ba0f1d3401cc41d2dd1
Ryan Delgrosso <ryandelgrosso@gmail.com>
2018-05-20 17:22:41 -0700

    Update channels.rb

--- lib/fsr/cmd/channels.rb
+++ lib/fsr/cmd/channels.rb
@@ -30,7 +30,7 @@ module FSR
             call_info, count = resp["body"].split("\n\n")
             require "fsr/model/channel"
             require "csv"
-            channels = CSV.parse(call_info) 
+            channels = CSV.parse(call_info, liberal_parsing: true) 
             headers = channels[0]
             @channels = channels[1 .. -1].map { |c| FSR::Model::Channel.new(headers ,*c) }
             return @channels
