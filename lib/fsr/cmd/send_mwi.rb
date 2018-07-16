907936ff09d0cd25f0bcdfc4f13e74bb3e4bdde3
root <root@pbx.siply.co>
2018-07-16 23:47:43 +0000

    Eliminating api prefix on send_mwi

--- lib/fsr/cmd/send_mwi.rb
+++ lib/fsr/cmd/send_mwi.rb
@@ -24,14 +24,14 @@ module FSR
 
       # Send the command to the event socket, using api by default.
       def run(api_method = :api)
-        orig_command = "%s %s" % [api_method, raw]
+        orig_command = "%s %s" % [raw]
         Log.debug "saying #{orig_command}"
         @fs_socket.say(orig_command)
       end
     
       # This method builds the API command to send to the freeswitch event socket
       def raw
-        orig_command = "sendevent message_waiting, #{@options}"
+        orig_command = "sendevent message_waiting" @options
       end
     end
 
